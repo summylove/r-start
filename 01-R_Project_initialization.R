b453f2b7d1eb959446849e1bce6b0da668295af8
summylove <ilili163163163@163.com>
2020-07-23 07:09:22 +0800

    01

---
+++ 01-R_Project_initialization.R
@@ -0,0 +1,22 @@
+rm(list=ls())
+
+load("D:/projects/paper1/revised2/revised2/genemodule_AF.Rdata")
+
+options(stringsAsFactors = FALSE) #禁止chr转成factor
+Sys.setlocale("LC_ALL","English") #Sys.setenv(LANGUAGE = "en") 
+#Sys.setlocale("LC_COLLATE", "C")
+options(encoding = "utf-8")
+
+pl=c('GEOquery','ggplot2') 
+lapply(pl, function(i){
+  if (!require(i,character.only = TRUE, quietly = TRUE)){
+    BiocManager::install(i,character.only = TRUE)
+  }  
+  if (!require(i,character.only = TRUE, quietly = TRUE)){
+    install.packages(i,character.only = TRUE)
+  } 
+  print(i,character.only = TRUE)
+  require(i, character.only = TRUE)
+})
+(.packages())
+
