5d747645df6b564be65dc3808c614e39668de4df
summylove <ilili163163163@163.com>
2020-08-03 14:25:18 +0800

    05

---
+++ 05-Rbooks_online.R
@@ -0,0 +1,22 @@
+##编程 programming
+#https://www.math.pku.edu.cn/teachers/lidf/docs/Rbook/html/_Rbook/index.html  #不仅仅正则表达式
+#https://bookdown.org/tonykuoyj/eloquentr/ #认识R 的美好 郭耀仁
+#http://www.datasciencemadesimple.com/r-tutorial/ #R编程教程–初学者，中级和高级
+#https://openbiox.github.io/Cookbook-for-R-Chinese/ #Cookbook for R 中文版
+#https://adv-r.hadley.nz/s4.html #Chapman＆Hall R系列的第二本书“ Advanced R ”的网站
+
+##统计 statistics
+#https://r4ds.had.co.nz/ #“ R for Data Science”的网站
+#https://rafalab.github.io/dsbook/ #数据科学导论
+#https://guangchuangyu.github.io/statistics_notes/index.html # 统计笔记 余光创
+#https://stat.ethz.ch/~meier/teaching/anova/ #ANOVA：使用R的简短介绍
+#https://www.statmethods.net/advstats/index.html #更高级的统计方法。发现和探索变量之间复杂的多元关系。
+#https://mb3is.megx.net/gustame/home/why-multivariate-analysis #多元方法的非技术性说明、微生物生态学中使用的各种分析技术的说明
+
+##流程 workflows
+#https://www.stephaniehicks.com/2018-bioinfosummer-scrnaseq/index.html #单细胞RNA序列数据分析：2018 BioInfoSummer Workshop
+#http://qiubio.com/new/book/chapter-02/#%E5%86%85%E5%AE%B9content #使用Bioconductor成熟软件包分析基因芯片数据，测序数据等
+
+## 可视化
+#https://jokergoo.github.io/circlize_book/book/  #R中的环形可视化 祖光谷
+#https://wencke.github.io/ #GOplot软件包专注于生物数据的可视化。更准确地说，该软件包将帮助将表达数据与功能分析的结果进行合并和整合。
